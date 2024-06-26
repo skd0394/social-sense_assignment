d665e3591bd848d75b1401feb720d1e8fc598558
shivkant dubey <shivkd1908@gmail.com>
2024-06-26 16:57:39 +0530

    added button for routes

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
+import { Button } from "@/components/ui/button";
 import Frame1 from "./frame1/page";
 import Frame2 from "./frame2/page";
-
 
 export default function Home() {
   return (
     <>
       {/* <Frame1/> */}
-      <Frame2/>
+      {/* <Frame2/> */}
+      <a href="/frame1">
+        <Button>Frame1</Button>
+      </a>
+      <a href="/frame2">
+        <Button>frame2</Button>
+      </a>
     </>
   );
 }
